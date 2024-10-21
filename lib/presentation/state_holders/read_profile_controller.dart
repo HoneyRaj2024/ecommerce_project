1fe8a3dfe1fd49aec6818ca39a5f9fcda37ee447
HoneyRaj2024 <alsaadit2023@gmail.com>
2024-10-21 11:09:27 +0600

    Initial commit

---
+++ lib/presentation/state_holders/read_profile_controller.dart
@@ -0,0 +1,43 @@
+import 'package:ecommerce_project/data/models/network_response.dart';
+import 'package:ecommerce_project/data/services/network_caller.dart';
+import 'package:ecommerce_project/data/utils/urls.dart';
+import 'package:ecommerce_project/presentation/state_holders/auth_controller.dart';
+import 'package:get/get.dart';
+
+class ReadProfileController extends GetxController {
+  bool _inProgress = false;
+
+  bool _isProfileCompleted = false;
+
+  bool get inProgress => _inProgress;
+
+  bool get isProfileCompleted => _isProfileCompleted;
+
+  String? _errorMessage;
+
+  String? get errorMessage => _errorMessage;
+
+  Future<bool> getProfileDetails(String token) async {
+    bool isSuccess = false;
+    _inProgress = true;
+    update();
+    final NetworkResponse response = await Get.find<NetworkCaller>().getRequest(
+      url: Urls.readProfile,
+      token: token,
+    );
+
+    if (response.isSuccess) {
+      if (response.responseData['data'] != null) {
+        _isProfileCompleted = true;
+        await Get.find<AuthController>().saveAccessToken(token);
+      }
+      isSuccess = true;
+      _errorMessage = null;
+    } else {
+      _errorMessage = response.errorMessage;
+    }
+    _inProgress = false;
+    update();
+    return isSuccess;
+  }
+}
